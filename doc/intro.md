a607b388cd8c7dfb292550f305f1613f4b6f6c5b
Joseph Stevens <josephscottstevens@gmail.com>
2018-09-24 14:01:33 -0400

    initial commit from app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-homework-jss
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_homework_jss/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-homework-jss.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
