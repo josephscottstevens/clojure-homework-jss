70cdfa3b6c9439c9865d9c99e1cbcd42b697850a
Joseph Stevens <josephscottstevens@gmail.com>
2018-09-25 09:33:12 -0400

    added torecord method

--- src/clojure_homework_jss/core.clj
+++ src/clojure_homework_jss/core.clj
@@ -1,9 +1,12 @@
 (ns clojure-homework-jss.core
   (:require [clojure.java.io :as io]))
 
+(defn toRecord [str]
+  [str])
+
 (def x
   (with-open [rdr (io/reader "./resources/data.txt")]
-    (doall (line-seq rdr))))
+    (doall (map toRecord (line-seq rdr)))))
 
 (defn -main
   "Reads file from command line, and prints to screen"
