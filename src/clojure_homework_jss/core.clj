e6d8419e53b94ed53e1dbc1b1f3166afec44b9e1
Joseph Stevens <josephscottstevens@gmail.com>
2018-09-25 09:46:06 -0400

    maps specific fields improved some names

--- src/clojure_homework_jss/core.clj
+++ src/clojure_homework_jss/core.clj
@@ -3,13 +3,19 @@
             [clojure.string :as string]))
 
 (defn toRecord [str]
-  {:a (string/split str #"\|")})
+  (def strArray
+    (string/split str #"\|"))
+  {:last-name (nth strArray 0)
+   :first-name (nth strArray 1)
+   :gender (nth strArray 2)
+   :favorite-color (nth strArray 3)
+   :date-of-birth (nth strArray 4)})
 
-(def x
+(def read-file
   (with-open [rdr (io/reader "./resources/data.txt")]
     (doall (map toRecord (line-seq rdr)))))
 
 (defn -main
   "Reads file from command line, and prints to screen"
   [& args]
-  (println x))
+  (println read-file))
